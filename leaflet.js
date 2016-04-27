5ede5788a2fc7f4072f71d02d585226e5d9884a2
Daniel Juanes Quintana <d.juanes@alumnos.urjc.es>
2016-04-27 02:19:42 +0200

    Realizacion de la practica v1.0

--- leaflet.js
+++ leaflet.js
@@ -1,43 +1,76 @@
-$(document).ready(function(){
+var mymap;
+var marker = null;
+var address;
 
-  var mymap = L.map('map');
+var loadPhotos = function() {
+  $("#direcciones").empty();
+  $("#pict").empty();
+  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+  $.getJSON( flickerAPI, {
+    tags: address,
+    tagmode: "any",
+    format: "json"
+  })
+  .done(function( data ) {
+    console.log("Success");
+    if(data.stat == "fail"){
+
+    }
+    $.each( data.items, function( i, item ) {
+      $("#pict").append('<img src="'+item.media.m+' "/>');
+      if ( i === 3 ) {
+        return false;
+      }
+    });
+  });
+};
+
+var loadResult = function(items){
+  $("#direcciones").empty();
+  if (items.length != 0) {
+    $('<p>', { html: "Search results:" }).appendTo('#direcciones');
+    $('<ul/>', {
+      'class': 'my-new-list',
+      html: items.join('')
+    }).appendTo('#direcciones');
+  } else {
+    $('<p>', { html: "No results found" }).appendTo('#direcciones');
+  }
+};
+
+var searchAddress = function(){
+  address = $("#direccion").val();
+  console.log(address);
+
+  var items = [];
+  var i = 0;
+  $.getJSON('http://nominatim.openstreetmap.org/search?format=json&limit=5&q=' + address, function(data) {
+    $.each(data, function(key, val) {
+      items[i]=( "<li><a href='#' onclick='rightAddress(" + val.lat + ", " + val.lon + ");return false;'>" + val.display_name +'</a></li>');
+      console.log(items[i]);
+      i++;
+    });
+    loadResult(items);
+  });
+};
+
+var rightAddress = function(lat, long){
+  marker = L.marker([lat, long]);
+  marker.addTo(mymap);
+  mymap.panTo(new L.LatLng(lat, long));
+  mymap.setZoom(16);
+  loadPhotos();
+};
+
+
+$(document).ready(function(){
+  mymap = L.map('map');
+
   mymap.locate({setView: true, maxZoom: 16});
 
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
+	   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(mymap);
 
-
-
-  var marker = L.marker([40.2838, -3.8215]);
-  marker.bindPopup("URJC").openPopup();
-  marker.addTo(mymap);
-
-  function onMapClick(e) {
-      var popup = L.popup();
-      popup
-          .setLatLng(e.latlng)
-          .setContent(e.latlng.toString())
-          .openOn(mymap);
-  };
-
-  mymap.on('click', onMapClick);
-
-  function onLocationFound(e) {
-    var radius = e.accuracy / 2;
-
-    L.marker(e.latlng).addTo(mymap)
-        .bindPopup("You are within " + radius + " meters from this point").openPopup();
-
-    L.circle(e.latlng, radius).addTo(mymap);
-  };
-
-  function onLocationError(e) {
-    alert(e.message);
-  };
-
-  mymap.on('locationfound', onLocationFound);
-  mymap.on('locationerror', onLocationError);
-
-
+  $("#send").click(searchAddress);
 });
